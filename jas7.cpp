158a9a3be8f2c9da1bf2285b4be27425180c6b30
Jasmine Princy Lobo <jasminelobo49@gmail.com>
2018-07-17 10:34:09 +0530

    Add files via upload

---
+++ jas7.cpp
@@ -0,0 +1,12 @@
+//to find ASCII value of a character
+#include<iostream>
+using namespace std;
+int main()
+{
+char c;
+cout<<"enter a character:"<<endl;
+cin>>c;
+cout<<"ASCII value  of"<<c<<" is:"<<int(c)<<endl;
+
+return 0;
+}
